ba45726be6e2c0cadbdebc0ef78b521660e64aaf
handawoon0830 <handawoon0830@naver.com>
2023-11-05 20:55:54 +0900

    2-2

--- main.c
+++ main.c
@@ -6,13 +6,19 @@
 
 int main(int argc, char *argv[]) {
 	int grade[N_STUDENT];
-	int i;
+	int i, average;
+	int sum;
 	
 	printf("input 5 scores:");
-	for (i=0; i<5; i++)
-	scanf("%d",&grade[i]);
+	sum=0;
 	
-	for (i=0;i<5; i++)
-	printf("grade[%d]= %i\n",i,grade[i]);
+	for (i=0; i<5; i++){
+	scanf("%d",&grade[i]);
+	sum+= grade[i];
+	}
+	
+	average= sum/ N_STUDENT;
+	
+	printf("score average: %i\n", average);
 	return 0;
 }
