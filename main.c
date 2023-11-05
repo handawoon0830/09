02553ea687cc606fea8010e7ff2881e138477d9b
handawoon0830 <handawoon0830@naver.com>
2023-11-05 20:52:04 +0900

    2

--- main.c
+++ main.c
@@ -6,14 +6,13 @@
 
 int main(int argc, char *argv[]) {
 	int grade[N_STUDENT];
-	
-	grade[0]= 10;
-	grade[1]= 20;
-	grade[2]= 30;
-	grade[3]= 40;
-	grade[4]= 50;
 	int i;
+	
+	printf("input 5 scores:");
 	for (i=0; i<5; i++)
-	printf("grade[%i]= %i\n",i,grade[i]);
+	scanf("%d",&grade[i]);
+	
+	for (i=0;i<5; i++)
+	printf("grade[%d]= %i\n",i,grade[i]);
 	return 0;
 }
